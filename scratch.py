661ebbe90761b4ad3cb5518b6c1ae4c52e281c6c
thomasambiz <thomasambiz@gmail.com>
2015-07-30 04:20:06 +0000

    revised bartender game

--- scratch.py
+++ scratch.py
@@ -1,4 +1,6 @@
+
 import random
+
 questions = {
     "strong": "Do ye like yer drinks strong?",
     "salty": "Do ye like it with a salty tang?",
@@ -6,6 +8,7 @@ questions = {
     "sweet": "Would ye like a bit of sweetness with yer poison?",
     "fruity": "Are ye one for a fruity finish?"
 }
+
 ingredients = {
     "strong": ["a glug of tequilla", "a slug of whisky", "a splash of gin"],
     "salty": ["an olive on a stick", "a salt-dusted rim", "a rasher of bacon"],
@@ -13,18 +16,23 @@ ingredients = {
     "sweet": ["a sugar cube", "a spoonful of honey", "a spash of cola"],
     "fruity": ["a slice of orange", "a dash of cassis", "a cherry on top"]
 }
+
 like = {'strong': False, 'salty': False, 'bitter': False, 'sweet': False, 'fruity': False}
+
 def ask():
     print "I be a pirate what can make a smashin drink.  Just lem'me know what ye be likin'.  I'm as daft me peg leg, so please keep yer answers simple... like 'yes' or 'no'"
     for key in questions:
         ans = raw_input(questions[key])
         if ans == "yes":
             like[key] = True
+
 def answer():   
     mix_li = []
     for key in like:
         if like[key] == True:
             mix_li.append(random.choice(ingredients[key]))
+    
+            
     print "\n Yer gonna love this, it has:\n"
     print(", ".join(mix_li))
     
